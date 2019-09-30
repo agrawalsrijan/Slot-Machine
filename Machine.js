6370744aba943bcbd0d3224ac3808f26b8d37973
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-09-30 13:29:36 +0530

    Adding styles
    Little bit of styling added

--- Machine.js
+++ Machine.js
@@ -3,10 +3,13 @@ class Machine extends React.Component {
 		const {s1,s2,s3} = this.props;
 		// set winner to either true or false
 		const winner = (s1===s2) && (s2===s3);
+		// creating an object that has styles and then using it inline
+		const colors = {fontSize:"60px"};
 		return(
-			<div>
-				<h1>{s1} {s2} {s3}</h1>
-				<h2>{winner ? "You win!" : "You lose"}</h2> 
+			// adding styles externally and inline
+			<div className="Machine">
+				<h1 style={colors}>{s1} {s2} {s3}</h1>
+				<h2 className={winner?"win":"lose"}>{winner ? "You win!" : "You lose"}</h2> 
 			</div>
 		);
 	}
